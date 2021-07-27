0056200dd34435daf30e6c01887f3fb36912356d
Josue Hiram Alvarez Martinez <josue.h.alvarez.martinez@gmail.com>
2021-07-27 14:29:54 -0500

    Finalización de la aplicación

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -12,5 +13,62 @@ export class NoComunesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  //i18nSelect
+  nombre: string = 'Josue';
+  genero: string = 'masculino';
+  invitacionMapa = {
+    'femenino' : 'invitarla',
+    'masculino' : 'invitarlo'
+  }
+
+  //i18nPlural
+  Clientes: string[] = ['Josue', 'Fernando', 'Daniel'];
+  clientesmapa = {
+    '=0' : 'no tenemos ningún cliente esperando.',
+    '=1' : 'tenemos un   cliente esperando.',
+    'other' : 'tenemos # clientes esperando.'
+  }
+
+  cambiarCliente(){
+    this.nombre = 'Marisol';
+    this.genero = 'femenino';
+  }
+
+  borrarCliente(){
+    this.Clientes.shift();
+  }
+
+  //KeyValue Pipe
+  persona = {
+    nombre: 'Josue Hiram',
+    edad: 20,
+    direccion: 'Zapopan, Jalisco.'
+  }
+
+  //JsonPipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Batman',
+      vuela: false
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    }
+  ];
+
+  //Async Pipe
+  myObservable = interval(2000);
+
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa.');
+    }, 3500);
+  });
 
 }
--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,6 +8,41 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class OrdenarComponent implements OnInit {
+
+  EnMayusculas : boolean = true;
+  heroes: Heroe[] = [
+    {
+    nombre:'Superman',
+    vuela: true,
+    color: Color.azul
+    },
+    {
+      nombre:'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre:'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre:'IronMan',
+      vuela: true,
+      color: Color.rojo
+    }
+  ];
+
+  ordenarPor: string = '';
+
+
+  mayusculas(){
+    this.EnMayusculas = !this.EnMayusculas;
+  }
+
+  cambiarorden(valor: string){
+    this.ordenarPor = valor;
+  }
 
   constructor() { }
 
